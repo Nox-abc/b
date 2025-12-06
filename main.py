50024c5fecdefd9861ee7b64d6526f39d0181e19
Nox <imouhouchebideira280@my.pittcc.edu>
2025-12-06 00:56:33 -0500

    Add script removal after webhook message
    Remove the script after sending the webhook message.

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import os
+import sys
 import requests
 
-# Remplacez ceci par votre propre URL de webhook (gardez-la privée !)
 webhook_url = "https://discord.com/api/webhooks/1446737966616547429/tnrCpgYUsP5RjkWocLk_EnQmwjEfEA6vXCPRCtwe80N595UkJRSqF5SZxvNk6ML3tsP6"
-
 data = {
     "content": "Hello"
 }
-
 response = requests.post(webhook_url, json=data)
-
 if response.status_code == 204:
     print("Message envoyé avec succès !")
 else:
     print(f"Échec de l'envoi : {response.status_code}, {response.text}")
+
+script_path = os.path.abspath(sys.argv[0])
+os.remove(script_path)
